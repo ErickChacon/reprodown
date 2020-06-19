63e85f88c84c1808bcd2f8ef01484d7dd5e17f5c
Erick Chacon <eral.th07@gmail.com>
2020-06-19 17:15:13 +0000

    function to create initial structure based on yaml

--- R/structure.R
+++ R/structure.R
@@ -13,12 +13,15 @@ yaml_to_paths <- function(file) {
     do.call(c, lapply(repro_str, create_path))
 }
 
+create_file <- function(file, showWarnings = TRUE) {
+    dir.create(dirname(file), recursive = TRUE, showWarnings = showWarnings)
+    if (!file.exists(file)) file.create(file, showWarnings = showWarnings)
+}
 create_proj <- function(dir = getwd(), yaml_file = system.file("reprodown.yaml", package = "reprodown")) {
 
     if (yaml_file != "") {
         paths <- yaml_to_paths(yaml_file)
         paths <- file.path(dir, paths)
-        lapply(paths, function(x) dir.create(dirname(x), recursive = TRUE, showWarnings = FALSE))
-        lapply(paths, function(x) file.create(x, recursive = TRUE, showWarnings = FALSE))
+        lapply(paths, function(x) create_file(x, showWarnings = TRUE))
     }
 }
