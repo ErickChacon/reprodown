09ea5013905e30e06e308f0ee2b6be2080e77e53
Erick Chacon <eral.th07@gmail.com>
2020-09-09 15:59:50 +0100

    export create_proj

--- R/structure.R
+++ R/structure.R
@@ -17,8 +17,26 @@ create_file <- function(file, showWarnings = TRUE) {
     dir.create(dirname(file), recursive = TRUE, showWarnings = showWarnings)
     if (!file.exists(file)) file.create(file, showWarnings = showWarnings)
 }
-create_proj <- function(dir = getwd(), yaml_file = system.file("reprodown.yaml", package = "reprodown")) {
 
+#' @title Create folders and files for the project
+#'
+#' @description
+#' \code{create_proj} creates folders based on the structure of a "yaml_file". By default
+#' it is the structure of the file \code{system.file("reprodown.yaml", package =
+#' "reprodown")}.
+#'
+#' @details
+#' For the yaml_file. Each bullet item is understood as a folder, its corresponding
+#' sub-items are considered sub-folders.
+#'
+#' @param dir Directory where to create our project. By default, this is the current
+#' working directory.
+#' @param yaml_file A file that defines the structure of the project.
+#'
+#' @author Erick A. Chacón-Montalván
+#'
+#' @export
+create_proj <- function(dir = getwd(), yaml_file = system.file("reprodown.yaml", package = "reprodown")) {
     if (yaml_file != "") {
         paths <- yaml_to_paths(yaml_file)
         paths <- file.path(dir, paths)
